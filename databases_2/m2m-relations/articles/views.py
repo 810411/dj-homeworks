602539bd1e0dd56443df801df13b75bebd6ee6fc
Grigorii Balabanov <810411@mail.ru>
2019-04-06 16:25:28 +0500

    databases_2 done

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,9 +1,27 @@
 from django.views.generic import ListView
 
-from articles.models import Article
+from articles.models import Article, ArticleScope
 
 
 class ArticleListView(ListView):
     template_name = 'articles/news.html'
     model = Article
     ordering = '-published_at'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        object_list = []
+
+        for article in context['object_list']:
+            scopes_list = []
+
+            for item in ArticleScope.objects.filter(article=article).all():
+                scopes_list.append({'topic': item.scopes.topic, 'is_main': item.is_main})
+
+            article.scopes_all = sorted(scopes_list, key=lambda x: x['topic'] if not x['is_main'] else 'a')
+
+            object_list.append(article)
+
+        context['object_list'] = object_list
+
+        return context
--- databases_2/orm_query/articles/views.py
+++ databases_2/orm_query/articles/views.py
@@ -7,3 +7,6 @@ class ArticleListView(ListView):
     template_name = 'articles/news.html'
     model = Article
     ordering = '-published_at'
+
+    def get_queryset(self):
+        return self.model.objects.defer("published_at").select_related("author", "genre")
