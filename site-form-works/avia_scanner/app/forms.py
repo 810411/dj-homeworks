aced10095e51cad9790f30c3858a172935c9ab63
Grigorii Balabanov <810411@mail.ru>
2019-04-08 21:59:26 +0500

    site-form-works done

--- site-form-works/avia_scanner/app/forms.py
+++ site-form-works/avia_scanner/app/forms.py
@@ -7,16 +7,13 @@ from .models import City
 class SearchTicket(forms.Form):
     departure_city = forms.CharField(
         label='Город отправления',
-        label_suffix=':',
         widget=AjaxInputWidget(url='api/city_ajax', attrs={'class': 'inline right-margin'})
     )
     arrival_city = forms.ModelChoiceField(
         queryset=City.objects.all(),
-        label='Город прибытия',
-        label_suffix=':'
+        label='Город прибытия'
     )
     data = forms.DateField(
         label='Дата',
-        label_suffix=':',
         widget=forms.SelectDateWidget()
     )
