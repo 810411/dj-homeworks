dcef6b642e700ea6520325bdb1c1307b9f58a713
Grigorii Balabanov <810411@mail.ru>
2019-04-12 00:37:16 +0500

    site-personalisation done

--- site-personalization/paid_content/articles/views.py
+++ site-personalization/paid_content/articles/views.py
@@ -1,15 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import ProfileRegisterForm
+from .models import Article
 
 
 def show_articles(request):
+    is_logged = request.user.is_authenticated
+    has_subscription = False
+    articles = Article.objects.filter(by_subscription=False).values('pk', 'title')
+
+    if is_logged:
+        has_subscription = request.user.has_subscription
+
+        if has_subscription:
+            articles = Article.objects.all().values('pk', 'title')
+
+    context = {
+        'articles': articles,
+        'is_logged': is_logged,
+        'has_subscription': has_subscription
+    }
+
     return render(
         request,
-        'articles.html'
+        'articles.html',
+        context
     )
 
 
 def show_article(request, id):
+    article = Article.objects.get(pk=id)
+
+    context = {
+        'article': article
+    }
+
     return render(
         request,
-        'article.html'
+        'article.html',
+        context
     )
+
+
+def subscribe(request):
+    user = request.user
+
+    user.has_subscription = True
+    user.save()
+
+    return redirect(reverse('articles'))
+
+
+def signup(request):
+    if request.method == 'POST':
+        register_form = ProfileRegisterForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+            return redirect(reverse('login'))
+    else:
+        register_form = ProfileRegisterForm()
+
+    context = {'form': register_form}
+
+    return render(request, 'registration/signup.html', context)
--- site-personalization/paid_content/main/urls.py
+++ site-personalization/paid_content/main/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from django.urls import path
 
-import views
+from articles import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', views.show_articles),
-    url(r'^articles/(?P<id>[0-9]+)/', views.show_article),
+    path('articles/', views.show_articles, name='articles'),
+    path('articles/<int:id>/', views.show_article, name='article'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('', RedirectView.as_view(url='articles/')),
 ]
